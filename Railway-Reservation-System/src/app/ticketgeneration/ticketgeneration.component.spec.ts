cd9692844a3098f96274e6efdf49e1290c8bf8d3
Kadambaree Maske <85875195+Kadambaree@users.noreply.github.com>
2023-05-14 19:13:50 +0530

    Add files via upload

---
+++ Railway-Reservation-System/src/app/ticketgeneration/ticketgeneration.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TicketgenerationComponent } from './ticketgeneration.component';
+
+describe('TicketgenerationComponent', () => {
+  let component: TicketgenerationComponent;
+  let fixture: ComponentFixture<TicketgenerationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TicketgenerationComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TicketgenerationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
